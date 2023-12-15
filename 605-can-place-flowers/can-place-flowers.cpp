541e4227f3f5438fa243d0cb4171f40059a72fea
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2023-12-15 11:36:35 +0530

    Time: 11 ms (79.05%) | Memory: 20.7 MB (60.66%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int oddplace = 0, evenplace = 0;
-        int size = flowerbed.size();
-        if(size == 1 && flowerbed[0] == 0 && n <= 1) return true;
-        for(int i = 1; i < size; i+=2){
-            if(i == size-1 && flowerbed[i] == 0 && flowerbed[i-1] == 0) oddplace++;
-            else if(i < size && flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0)  oddplace++;
+        if(n == 0){
+            return true;
         }
-        for(int i = 0; i < size; i+=2){
-            if(i == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0){
-                evenplace++;
+        int sz = flowerbed.size();
+        for(int i = 0; i < sz; i++){
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == sz-1 || flowerbed[i+1] == 0)){
+                i++;
+                n--;
             }
-            else if(i == size - 1 && flowerbed[i] == 0 && flowerbed[i-1] == 0) evenplace++;
-            else if(i > 0 && flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0){
-                evenplace++;
-            }
+            if(!n) return true;
         }
-
-        return oddplace >= n || evenplace >= n;
+        return false;
     }
 };
