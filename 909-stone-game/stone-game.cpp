a11b066ccc7865550073063ac3ded778ba8f58b7
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-01-03 18:27:08 +0530

    Time: 22 ms (32.62%) | Memory: 16.8 MB (22.23%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-        return true;
+    bool stoneGame(vector<int>& A) {
+        int N=A.size();
+        vector<vector<int>>dp(N,vector<int>(N,0));
+        for(int i=0; i<N; i++){
+            for(int j=0,k=i; k<N; k++){
+            if(i==0){
+                dp[j][k]=A[j];
+            }
+            else if(i==1){
+                dp[j][k]=max(A[j],A[k]);
+            }
+            else{
+                int v1=A[j]+min(dp[j+2][k],dp[j+1][k-1]);
+                int v2=A[k]+min(dp[j+1][k-1],dp[j][k-2]);
+                dp[j][k]=max(v1,v2);
+            }
+            }
+        }
+        return dp[0][N-1];
     }
 };
