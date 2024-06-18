795a29c0298f946c27c63a4118cd441d35b95a6f
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-06-18 23:59:49 +0530

    Time: 99 ms (38.87%) | Memory: 52.7 MB (89.38%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    vector<vector<string>>res;
-    bool isPali(string&s){
-        int l=0,r=s.length()-1;
-        while(l<r){
-            if(s[l]!=s[r]){
-                return false;
-            }
-            l++,r--;
-        }
-        return true;
-    }
-    void solve(int l,string &s, vector<string>&st){
-        if(l==s.length()){
-            res.push_back(st);
-            return;
-        }
-        string pa="";
-        for(int i=l; i<s.length(); ++i){
-            pa+=s[i];
-            if(isPali(pa)){
-                st.push_back(pa);
-                solve(i+1,s,st);
-                st.pop_back();
-            }
-        }
-    }
     vector<vector<string>> partition(string s) {
-        vector<string> st;
-        solve(0,s, st);
-        return res;
+        int n = s.size();
+        bool f[n][n];
+        memset(f, true, sizeof(f));
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                f[i][j] = s[i] == s[j] && f[i + 1][j - 1];
+            }
+        }
+        vector<vector<string>> ans;
+        vector<string> t;
+        function<void(int)> dfs = [&](int i) {
+            if (i == n) {
+                ans.push_back(t);
+                return;
+            }
+            for (int j = i; j < n; ++j) {
+                if (f[i][j]) {
+                    t.push_back(s.substr(i, j - i + 1));
+                    dfs(j + 1);
+                    t.pop_back();
+                }
+            }
+        };
+        dfs(0);
+        return ans;
     }
 };
