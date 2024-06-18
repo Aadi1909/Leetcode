d899c115e05a70d5472718aa24f9da3d62009943
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-06-18 23:11:56 +0530

    Time: 674 ms (48.99%) | Memory: 10.8 MB (59.58%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < m; ++j) {
-                if(dfs(board, word, vis, i, j, 0)) {
-                    return true;
-                }
-            }
-        }
+bool solve(int i, int j, vector<vector<bool>>&vis, vector<vector<char>>&board, string&word, int n, int m, int k){
+
+    if(k == word.size()){
+        return true;
+    }
+      if(i >= n || j >= m || i < 0 || j < 0 || vis[i][j] || board[i][j] != word[k]){
         return false;
     }
-    
-private:
-    bool dfs(vector<vector<char>>& board, string& word, vector<vector<bool>>& vis, int i, int j, int k) {
-        if(k == word.size()) return true;
-        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || vis[i][j] || board[i][j] != word[k]) {
-            return false;
+    vis[i][j]=true;
+    bool res = solve(i+1, j, vis, board, word, n, m, k+1) || 
+               solve(i, j+1, vis, board, word, n, m, k+1) || 
+               solve(i-1, j, vis, board, word, n, m, k+1) || 
+               solve(i, j-1, vis, board, word, n, m, k+1);
+    vis[i][j]=false;
+    return res;
+}
+
+bool exist(vector<vector<char>>& board, string word) {
+    int n=board.size();
+    int m=board[0].size();
+    string str="";
+    bool ok = false;
+    vector<vector<bool>> vis(n,vector<bool> (m,false));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            bool re = solve(i, j, vis,board,word, n, m, 0);
+            if(re)return true;
         }
-        
-        vis[i][j] = true;
-        
-        bool found = dfs(board, word, vis, i+1, j, k+1) ||
-                     dfs(board, word, vis, i-1, j, k+1) ||
-                     dfs(board, word, vis, i, j+1, k+1) ||
-                     dfs(board, word, vis, i, j-1, k+1);
-        
-        vis[i][j] = false;
-        return found;
+    }
+        return false;
     }
 };
