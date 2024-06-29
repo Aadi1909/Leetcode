aad72641f267518efaca7993a37ae3ebe06c4949
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-06-29 15:42:21 +0530

    Time: 3 ms (57.06%) | Memory: 14.1 MB (42.88%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -8,14 +8,17 @@ public:
         }   
         while (!q.empty()) {
             int sz = q.size();
-            if (q.front() != nullptr) answer.push_back(q.front()->val);
+            //if (q.front() != nullptr) answer.push_back(q.front()->val);
             for (int i = 0; i < sz; ++i) {
                 TreeNode* node = q.front(); q.pop();
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
                 if (node->right != nullptr) {
                     q.push(node->right);
                 }
-                if (node->left != nullptr) {
-                    q.push(node->left);
+                if (i == sz-1) {
+                    answer.push_back(node->val);
                 }
             }
         }
