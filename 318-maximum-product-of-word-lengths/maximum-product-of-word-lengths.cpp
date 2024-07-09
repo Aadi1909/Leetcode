d64abddb0a908e8188b333c3b72e2947506325da
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-07-09 16:23:12 +0530

    Time: 51 ms (57.35%) | Memory: 25.7 MB (48.95%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
 int maxProduct(vector<string>& words) {
-    vector<string> a;
-    for(int i = 0; i < words.size(); ++i) {
-        string s(26, '0');
-        for(int j = 0; j < words[i].size(); ++j) {
-            int index = words[i][j] - 'a';
-            s[index] = '1';
-        }
-        a.push_back(s);
+		 vector<int> bit;
+		 for(int i = 0; i < words.size(); ++i){
+		  	int mask = 0;
+		  	for(int j = 0; j < words[i].length(); ++j){
+		  	 	int index = words[i][j] - 97;
+		  	 	mask = (mask | (1LL << index));
+		  	}
+		  	bit.push_back(mask);
+		 }
+		 int answer = 0;
+		 for(int i = 0; i < bit.size(); ++i){
+		  	for(int j = i + 1; j < bit.size(); ++j){
+		  	 	if((bit[i] & bit[j]) == 0){
+		  	 	 	answer = max(answer, (int)words[i].size() * (int)words[j].size());
+		  	 	}
+		  	}
+		 }
+	return answer;
     }
-
-    int answer = 0;
-    for(int i = 0; i < a.size(); ++i) {
-        for(int j = i + 1; j < a.size(); ++j) {
-            bool ok = true;
-            for(int k = 0; k < 26; ++k) {
-                if(a[i][k] == '1' && a[j][k] == '1') {
-                    ok = false;
-                    break;
-                }
-            }
-            if(ok) {
-                answer = max(answer, (int)words[i].length() * (int)words[j].length());
-            }
-        }
-    }
-    return answer;
-    }
-}; 
+};
