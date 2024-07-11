36cdf30e07a3ec5e2b69ed5fd915f62e69671e4e
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-07-11 15:38:12 +0530

    Time: 4 ms (31.32%) | Memory: 9.2 MB (35.38%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    int rob(vector<int>& a) {
-        int f=a[0],s=0;
-        for(int i=1; i<a.size(); i++){
-            int tmp=s;
-            s=max(f,s);
-            f=a[i]+tmp;
-        }
-        return max(f,s);
+    int func ( int i , int n , int dp [] , vector <int> & nums ) {
+        if ( i >= n ) return 0;
+        int answer = 0;
+        if ( dp [ i ] != -1 ) return dp [ i ];
+        answer = max ( answer, nums [ i ] + func ( i + 2 , n , dp , nums ) ) ;
+        answer = max ( answer, func ( i + 1 , n , dp , nums ) ) ;
+        return dp [ i ] = answer ;
+    }
+    int rob ( vector <int> & nums ) {
+        int n = nums.size ();
+        int dp [ n + 2 ];
+        memset ( dp , -1 , sizeof ( dp ) );
+        return func ( 0 , n , dp , nums );
     }
 };
