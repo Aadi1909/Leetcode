f45bdbb21dab0977fc99ac95bffcfb12af932e4b
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-07-12 14:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (8.70%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int countPaths=0;
-    int uniquePathsWithObstacles(vector<vector<int>>& A) {
-        int N=A.size(),M=A[0].size();
-
-        if(A[0][0]==1)return 0;
-        vector<vector<long long>>dp(N,vector<long long>(M,0));
-        dp[0][0]=1;
-        for(int i=1; i<N; i++){
-            if(A[i][0]!=1)dp[i][0]=dp[i-1][0];
-        }
-        for(int j=1; j<M; j++){
-            if(A[0][j]!=1)dp[0][j]=dp[0][j-1];
-        }
-        for(int i=1; i<N; i++){
-            for(int j=1; j<M; j++){
-                if(A[i][j]!=1) dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[N-1][M-1];
+    int uniquePathsWithObstacles ( vector < vector < int > > & Grid ) {
+        int n = Grid.size ( );
+        int m = Grid [ 0 ].size ( );
+        int dp [ n ] [ m ];
+        memset ( dp, -1, sizeof ( dp ) );
+        function < int ( int, int ) > sol = [ & ] ( int i , int j ) {
+            if ( Grid [ i ] [ j ] ) return 0;
+            if ( i == n - 1 and j == m - 1 ) return 1;
+            int answer = 0;
+            if ( dp [ i ] [ j ] != -1 ) return dp [ i ] [ j ];
+            if ( i + 1 < n ) answer += sol ( i + 1,  j ) ;
+            if ( j + 1 < m ) answer += sol ( i , j + 1 );
+            return dp [ i ] [ j ] = answer;
+        };
+        return sol ( 0 , 0 ) ;
     }
 };
