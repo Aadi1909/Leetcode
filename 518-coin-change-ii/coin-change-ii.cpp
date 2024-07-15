5017d7570bf9c148152bede88a8104dcac4ebc41
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-07-15 14:47:16 +0530

    Time: 5 ms (81.77%) | Memory: 13.5 MB (78.62%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        int m = amount;
-        if(amount == 0) return 1;
-        int dp[n][m];
-        memset(dp, -1, sizeof(dp));
-        function<int(int, int)> sol = [&](int currentAmount, int index){
-            if(index >= coins.size() or currentAmount > amount) return 0;
-            if(currentAmount == amount) return 1;
-            int answer = 0;
-            if(dp[index][currentAmount] != -1) return dp[index][currentAmount];
-            if((coins[index] + currentAmount) <= amount) 
-                answer += sol(currentAmount + coins[index], index);
-                answer += sol(currentAmount, index + 1);
-            return dp[index][currentAmount] = answer;
-        };
-        return sol(0,0);
+        int k = amount;
+        int dp[n + 1][k + 1];
+        memset(dp, 0, sizeof(dp));
+        dp[n][k] = 1;
+        for(int i = 0; i < k; ++i){
+            dp[n][i] = 0;
+        }
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = k; j >= 0; j--){
+                dp[i][j] = dp[i + 1][j];
+                if(j + coins[i] <= k){
+                    dp[i][j] += dp[i][j + coins[i]];
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
