cd71351991b5d3218db28d578e306fab2032b73d
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-10-21 23:30:01 +0530

    Time: 285 ms (35.38%) | Memory: 64.7 MB (36.55%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int maxUniqueSplit(string s) {
         set<string> st;
-        vector<string> v;
         int answer = 0;
         function<void(int)> sol = [&](int i) {
             if (i >= s.length()) {
@@ -13,10 +12,8 @@ public:
                 string sub = s.substr(i, j - i + 1); 
                 if (st.find(sub) == st.end()) {
                     st.insert(sub);
-                    v.push_back(sub);
                     sol(j + 1);
                     st.erase(sub); 
-                    v.pop_back();
                 }
             }
         };
