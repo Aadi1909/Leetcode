5bfbee77c1d6ee6d358c6bf245a0578ad9cff7ec
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-10-22 18:36:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (63.39%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.cpp
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.cpp
@@ -10,27 +10,22 @@ public:
             for(int i = 0; i < sz; ++i){
                 TreeNode* node = q.front(); q.pop();
                 if (node->left != nullptr && node->right != nullptr) {
-                    if ((node->left->val == x && node->right->val == y) ||
+                    if ((node->left->val == x && node->right->val == y) or
                         (node->left->val == y && node->right->val == x)) {
                         return false;
                     }
                 }
                 if(node->left != nullptr){
-                    if(node->left->val == x) val.push_back(node->val);
-                    else if(node->left->val == y) val.push_back(node->val);
+                    if(node->left->val == x or node->left->val == y) val.push_back(node->val);
                     q.push(node->left);
                 }
                 if(node->right != nullptr){
-                    if(node->right->val == x) val.push_back(node->val);
-                    else if(node->right->val == y) val.push_back(node->val);
+                    if(node->right->val == x or node->right->val == y) val.push_back(node->val);
                     q.push(node->right);
                 }
             }
             if(val.size() == 2){
                 if(val[1] != val[0]) return true;
-            }
-            else if(val.size() == 1){
-                return false;
             }
         }
         return false;
