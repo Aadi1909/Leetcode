5a8cfe1f5aeb0e99690651a5939075910b7d7d58
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-11-06 00:04:10 +0530

    Time: 10 ms (5.32%) | Memory: 12.1 MB (87.26%) - LeetSync

---
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minChanges(string s) {
+        int answer = 0;
+        for(int i = 1; i < s.length(); i+=2){
+            if(s[i] != s[i - 1]) answer++;
+        }    
+        return answer;
+    }
+};
