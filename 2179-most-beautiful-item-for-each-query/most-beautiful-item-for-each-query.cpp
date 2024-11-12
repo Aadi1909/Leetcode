89647506fad85b7c45904311fc2a3b4819c4c56c
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-11-12 19:15:32 +0530

    Time: 43 ms (95.90%) | Memory: 93.2 MB (71.96%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -3,15 +3,11 @@ public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
         int size = items.size();
         vector<int> prefMax(size);
-        vector<pair<int, int>> a(size);
-        for(int i = 0; i < size; ++i) {
-            a[i].first = items[i][0];
-            a[i].second = items[i][1];
-        }
-        sort(a.begin(), a.end());
-        prefMax[0] = a[0].second;
+        sort(items.begin(), items.end(), [](auto& l, auto& r){
+            return l[0] < r[0];            
+        });
         for(int i = 1; i < size; ++i) {
-            prefMax[i] = max(prefMax[i - 1], a[i].second);
+            items[i][1] = max(items[i - 1][1], items[i][1]);
         }
         vector<int> answer;
         for(int i = 0; i < queries.size(); ++i) {
@@ -19,8 +15,8 @@ public:
             int maxx = 0;
             while (lo <= hi) {
                 int mid = lo + (hi - lo) / 2;
-                if (a[mid].first <= queries[i]) {
-                    maxx = max(maxx, prefMax[mid]);
+                if (items[mid][0] <= queries[i]) {
+                    maxx = max(maxx, items[mid][1]);
                     lo = mid + 1;
                 } else {
                     hi = mid - 1;
