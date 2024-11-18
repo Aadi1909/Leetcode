f586c7123e43ddece409427c64357aa831a226ca
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-11-18 23:56:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (17.93%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -2,27 +2,19 @@ class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
         vector<int> result(code.size(), 0);
-        if(k == 0){
-            return result;
+        if (k == 0) return result;
+        int start = 1, end = k, sum = 0;
+        if (k < 0) {
+            start = code.size() - abs(k);
+            end = code.size() - 1;
         }
-        else if(k > 0){
-            for(int i = 0; i < code.size(); ++i){
-                int j = i + 1;
-                int round = k;
-                while(round--){
-                    result[i] += code[j%code.size()];
-                    j++;
-                }
-            }
-            return result;
-        }
-        else{
-            int n = code.size();
-            for (int i = 0; i < n; ++i) {
-                for (int j = 1; j <= -k; ++j) {
-                    result[i] += code[(i - j + n) % n];
-                }
-            }
+        for (int i = start; i <= end; i++) sum += code[i];
+        for (int i = 0; i < code.size(); i++) {
+            result[i] = sum;
+            sum -= code[start % code.size()];
+            sum += code[(end + 1) % code.size()];
+            start++;
+            end++;
         }
         return result;
     }
