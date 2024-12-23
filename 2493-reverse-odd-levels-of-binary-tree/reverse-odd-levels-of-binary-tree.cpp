fd2579d4afc2985f98836a319f08c9b8dd9db6b6
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-12-23 21:55:50 +0530

    Time: 6 ms (27.07%) | Memory: 84.2 MB (17.48%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
+        if (!root) {
+            return nullptr;
+        }
         queue<TreeNode*> q;
         q.push(root);
-        int level = 1;
+        int level = 0;
         while (!q.empty()) {
             int size = q.size();
-            queue<TreeNode*> qq = q;
-            vector<int> values;
-            for (int i = 0; i < size; ++i) {
-                TreeNode* node = q.front(); q.pop();
-                if (node->left != nullptr) {
-                    q.push(node->left);
-                    values.push_back(node->left->val);
-                }
-                if (node->right != nullptr) {
-                    q.push(node->right);
-                    values.push_back(node->right->val);
-                }
+            vector<TreeNode*> currentLevelNodes;
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                currentLevelNodes.push_back(node);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
             }
-            if (level & 1) {
-                for (int i = 0; i < size; ++i) {
-                    TreeNode* node = qq.front(); qq.pop();
-                    if (node->left != nullptr) {
-                        node->left->val = values.back();
-                        values.pop_back();
-                    }
-                    if (node->right != nullptr) {
-                        node->right->val = values.back();
-                        values.pop_back();
-                    }
+            if (level % 2 == 1) {
+                int left = 0, right = currentLevelNodes.size() - 1;
+                while (left < right) {
+                    int temp = currentLevelNodes[left]->val;
+                    currentLevelNodes[left]->val =
+                        currentLevelNodes[right]->val;
+                    currentLevelNodes[right]->val = temp;
+                    left++;
+                    right--;
                 }
             }
             level++;
