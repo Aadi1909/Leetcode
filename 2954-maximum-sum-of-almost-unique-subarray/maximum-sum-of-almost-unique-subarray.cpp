080cc9f0ad61467ff357a18c12f0b8dd75fa2a96
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2024-12-26 01:36:18 +0530

    Time: 56 ms (91.32%) | Memory: 70.2 MB (50.12%) - LeetSync

--- 2954-maximum-sum-of-almost-unique-subarray/maximum-sum-of-almost-unique-subarray.cpp
+++ 2954-maximum-sum-of-almost-unique-subarray/maximum-sum-of-almost-unique-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long maxSum(vector<int>& nums, int m, int k) {
-        map<int, int> mp;
+        unordered_map<int, int> mp;
         long long answer = 0;
         long long sum = 0;
         for (int i = 0; i < nums.size(); ++i) {
