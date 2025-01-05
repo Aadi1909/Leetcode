d95d69819d0ec088595bd728dc703d373ad56a2d
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2025-01-05 16:09:44 +0530

    Time: 12 ms (47.56%) | Memory: 98.2 MB (52.83%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -1,23 +1,46 @@
+class FenwickTree {
+private:
+    vector<int> tree;
+    int size;
+public:
+    FenwickTree(int n) : size(n), tree(n + 1, 0) {}
+    void update(int index, int delta) {
+        while (index <= size) {
+            tree[index] += delta;
+            index += index & -index;
+        }
+    }
+    int query(int index) {
+        int sum = 0;
+        while (index > 0) {
+            sum += tree[index];
+            index -= index & -index;
+        }
+        return sum;
+    }
+    void range_update(int left, int right, int delta) {
+        update(left, delta);
+        update(right + 1, -delta);
+    }
+};
 class Solution {
 public:
-    string shiftingLetters(std::string s, std::vector<std::vector<int>>& shifts) {
-        // can use difference array
-        int n = s.length();
-        std::vector<int> diff(n + 1, 0);
-        for(int i = 0; i < shifts.size(); ++i){
-            int l = shifts[i][0];
-            int r = shifts[i][1];
-            diff[l] += (shifts[i][2] ? 1 : -1);
-            diff[r + 1] += (shifts[i][2] ? -1 : 1);
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n = s.size();
+        FenwickTree fenwick(n);
+        for (const auto& shift : shifts) {
+            int start = shift[0] + 1;
+            int end = shift[1] + 1;
+            int direction = shift[2];
+            int delta = (direction == 1) ? 1 : -1;
+            fenwick.range_update(start, end, delta);
         }
-        partial_sum(diff.begin(), diff.end(), diff.begin());
-        int k = 26;
-        for(int i = 0; i < n; ++i){
-            int pos = s[i] - 'a';
-            pos += diff[i];
-            pos = (((pos % k) + k) % k);
-            s[i] = static_cast<char>(pos + 97);
-        }   
-        return s;
+        string result = s;
+        for (int i = 0; i < n; ++i) {
+            int net_shift = fenwick.query(i + 1) % 26;
+            if (net_shift < 0) net_shift += 26;
+            result[i] = 'a' + (s[i] - 'a' + net_shift) % 26;
+        }
+        return result;
     }
 };
