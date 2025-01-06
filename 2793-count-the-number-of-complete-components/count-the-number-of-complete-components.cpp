d26171d630ad5715ff258bc22d938e5eb40e1cff
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2025-01-06 14:34:43 +0530

    Time: 43 ms (75.14%) | Memory: 128.1 MB (74.68%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> g(n);
-        for (int i = 0; i < edges.size(); ++i) {
-            int x = edges[i][0];
-            int y = edges[i][1];
-            g[x].push_back(y);
-            g[y].push_back(x);
+        vector<vector<int>> graph(n);
+        for (const auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
         }
-        int count = 0;
-        set<int> st;
-        vector<bool> vis(n, false);
-        function<void(int, int&)> dfs = [&](int u, int& cnt) {
-            st.insert(g[u].size());
-            cnt++;
-            vis[u] = true;
-            for (int v : g[u]) {
-                if (!vis[v]) {
-                    dfs(v, cnt);
+        vector<bool> visited(n, false);
+        auto dfs = [&](int node, int& nodeCount, int& edgeCount, auto& dfs) -> void {
+            visited[node] = true;
+            nodeCount++;
+            edgeCount += graph[node].size();
+            for (int neighbor : graph[node]) {
+                if (!visited[neighbor]) {
+                    dfs(neighbor, nodeCount, edgeCount, dfs);
                 }
             }
         };
+        int answer = 0;
         for (int i = 0; i < n; ++i) {
-            if (!vis[i]) {
-                int k = 0;
-                dfs(i, k);
-                if (st.size() == 1 && *st.begin() == (k - 1)) count++;
-                st.clear();
+            if (!visited[i]) {
+                int nodeCount = 0, edgeCount = 0;
+                dfs(i, nodeCount, edgeCount, dfs);
+                if (edgeCount == nodeCount * (nodeCount - 1)) {
+                    answer++;
+                }
             }
         }
-        return count;
+        return answer;
     }
 };
