eba1788fda53971e69b40cfd28e9b20227f19125
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2025-01-12 15:34:21 +0530

    Time: 8 ms (43.08%) | Memory: 15.3 MB (8.00%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     bool canConstruct(string s, int k) {
-        // Handle edge cases
-        if (s.length() < k) return false;
-        if (s.length() == k) return true;
-
-        // Initialize frequency array and oddCount integer
-        vector<int> freq(26);
-        int oddCount = 0;
-
-        // Increment the value of the index corresponding to the current
-        // character
-        for (auto& chr : s) freq[chr - 'a']++;
-
-        // Count the number of characters that appear an odd number of times in
-        // s
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] % 2 == 1) {
-                oddCount++;
+        if(k > s.length()) return false;
+        std::vector<int> rem;
+        std::unordered_map<char, int> mp;
+        for(int i = 0; i < s.length(); ++i){
+            mp[s[i]]++;
+        }
+        bool last = false;
+        int count = 0;
+        for(auto& it : mp){
+            if(it.second % 2 == 1){
+                rem.push_back(it.first);
+                it.second--;
             }
         }
-
-        // Return if the number of odd frequencies is less than or equal to k
-        return (oddCount <= k);
+        return rem.size() > k ? false : true;
     }
 };
