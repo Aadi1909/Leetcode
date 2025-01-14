b6ed6b117873b81281672877ffb6b834ab1f19f9
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2025-01-14 16:02:08 +0530

    Time: 0 ms (100.00%) | Memory: 86.5 MB (60.69%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        set<int> setA, setB;
-        vector<int> answer(A.size(), 0);
-        for(int i = 0; i < A.size(); ++i){
-            set<int> intersection;
-            setA.insert(A[i]);
-            setB.insert(B[i]);
-            set_intersection(setA.begin(), setA.end(), setB.begin(), setB.end(),
-                            inserter(intersection, intersection.begin()));
-            answer[i] = intersection.size();
+    std::vector<int> findThePrefixCommonArray(std::vector<int>& A, std::vector<int>& B) {
+        std::vector<int> map(51, 0);
+        std::vector<int> answer;
+        answer.push_back(A[0] == B[0] ? 1 : 0);
+        if(A[0] == B[0]) map[A[0]]++;
+        else map[A[0]]++, map[B[0]]++;
+        for(int i = 1; i < A.size(); ++i){
+            if(A[i] == B[i]){
+                answer.push_back(answer.back() + 1);
+                if(map[A[i]] == 0) map[A[i]]++;
+            }
+            else {
+                int x = map[A[i]];
+                int y = map[B[i]];
+                answer.push_back(answer.back() + x + y);
+                if(map[A[i]] == 0)map[A[i]]++;
+                if(map[B[i]] == 0)map[B[i]]++;
+            }
         }
         return answer;
     }
