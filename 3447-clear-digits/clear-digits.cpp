d54e5ab3ab6c51c8a8543b5e3a87a70961bbf026
thisisadarsh <112549335+Aadi1909@users.noreply.github.com>
2025-02-10 15:49:35 +0530

    Time: 2 ms (20.65%) | Memory: 8.9 MB (49.32%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -2,11 +2,6 @@ class Solution {
 public:
     string clearDigits(string s) {
         int n = (int)s.length();
-        int ch = 0;
-        for (char& c : s) {
-            if (c >= 97 && c <= 122) ch++;
-        }
-        if (n - ch == ch) return "";
         stack<char> st;
         for (int i = 0; i < n; ++i) {
             if (s[i] >= 97 && s[i] <= 122) {
