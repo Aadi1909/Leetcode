285b4eeb5d1a57787abcba8c832c961ea510d4cb
Aadarsh Tyagi <112549335+Aadi1909@users.noreply.github.com>
2025-02-13 18:56:43 +0530

    Time: 55 ms (19.98%) | Memory: 52.9 MB (14.90%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) { 
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        function<int(int, int)> sol = [&](int i, int j) -> int {
-            if (j == amount) return 0; 
-            if (i >= n || j > amount) return 1e9;
-            if (dp[i][j] != -1) return dp[i][j];
-            int ans = sol(i + 1, j);
-            if (j + coins[i] * 1LL <= amount) {
-                ans = min(ans, 1 + sol(i, j + coins[i]));
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 1e9));
+        for (int j = 0; j <= n; ++j) dp[j][0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= amount; ++j) {
+                if (j >= coins[i - 1]) {
+                    dp[i][j] = min(dp[i][j], dp[i][j - coins[i - 1]] + 1);
+                }
+                dp[i][j] = min(dp[i][j], dp[i - 1][j]);
             }
-            return dp[i][j] = ans;
-        };
-        int ans = sol(0, 0);
-        return (ans == 1e9) ? -1 : ans;
+        }
+        return (dp[n][amount] == 1e9) ? -1 : dp[n][amount];
     }
 };
