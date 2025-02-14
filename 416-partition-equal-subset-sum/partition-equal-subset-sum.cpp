a756f63c2f9231668577099bd997a03337787754
Aadarsh Tyagi <112549335+Aadi1909@users.noreply.github.com>
2025-02-14 20:24:38 +0530

    Time: 135 ms (41.74%) | Memory: 101.1 MB (20.69%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -4,14 +4,24 @@ public:
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if(sum % 2 != 0) return false;
         int target = sum / 2;
-        vector<vector<int>> dp(nums.size(), vector<int>(target + 1, -1));
-        function<bool(int, int)> sol = [&](int i, int sof) {
-            if(sof == target) return true;
-            if(i >= nums.size() || sof > target) return false;
-            if(dp[i][sof] != -1) return (bool)dp[i][sof];
-            dp[i][sof] = sol(i + 1, sof) || sol(i + 1, sof + nums[i]);
-            return (bool)dp[i][sof];
-        };
-        return sol(0, 0);
+        vector<vector<int>> dp(nums.size() + 1, vector<int>(target + 1, 0));
+        dp[0][0] = true;
+        for(int i = 0; i <= nums.size(); ++i) dp[i][0] = true;
+        for(int i = 1; i <= nums.size(); ++i){
+            for(int j = 1; j <= target; ++j){
+                if(j >= nums[i-1])
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                dp[i][j] |= dp[i - 1][j];
+            }
+        }
+        return dp[nums.size()][target];
     }
 };
+        // function<int(int, int)> sol = [&](int i, int sof) {
+        //     if(sof == target) return 1;
+        //     if(i >= nums.size() || sof > target) return 0;
+        //     if(dp[i][sof] != -1) return dp[i][sof];
+        //     dp[i][sof] = sol(i + 1, sof) || sol(i + 1, sof + nums[i]);
+        //     return dp[i][sof];
+        // };
+        // return sol(0, 0);
