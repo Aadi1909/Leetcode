6b512e2b510a6b60ac84d83c97cf20bee4cb0d39
Aadarsh Tyagi <112549335+Aadi1909@users.noreply.github.com>
2025-03-03 20:17:33 +0530

    Time: 0 ms (100.00%) | Memory: 60.9 MB (88.40%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -1,43 +1,76 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> bits(n);
-        for(int j = 0; j < n; ++j) {
-            for(int i = 0; i <= 31; ++i) {
-                if (nums[j] & (1 << i)) 
-                    bits[j].push_back(i);
-            }
-        }
-        int max_length = 1;
-        set<int> st(bits[0].begin(), bits[0].end()); 
+        int max_length = 0;
+        int mask = 0;
         int l_o = 0;
-        for(int i = 1; i < n; ++i) {
-            vector<int> new_bits = bits[i];
-            bool is_valid = true;
-            for (int new_bit : new_bits) {
-                if (st.find(new_bit) != st.end()) {
-                    is_valid = false;
-                    break;
-                }
+        for (int i = 0; i < nums.size(); ++i) {
+            while ((mask & nums[i]) != 0) {
+                mask ^= nums[l_o];
+                l_o++;
             }
-            while (!is_valid) {
-                for (int rm : bits[l_o]) 
-                    st.erase(rm);
-                    l_o++;
-                    is_valid = true;
-                    for (int new_bit : new_bits) {
-                        if (st.find(new_bit) != st.end()) {
-                            is_valid = false;
-                            break;
-                        }
-                }
-            }
-            for (int new_bit : new_bits) 
-                st.insert(new_bit);
+            mask |= nums[i];
             max_length = max(max_length, i - l_o + 1);
         }
 
         return max_length;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int longestNiceSubarray(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> bits(n);
+//         for(int j = 0; j < n; ++j) {
+//             for(int i = 0; i <= 31; ++i) {
+//                 if (nums[j] & (1 << i)) 
+//                     bits[j].push_back(i);
+//             }
+//         }
+//         int max_length = 1;
+//         set<int> st(bits[0].begin(), bits[0].end()); 
+//         int l_o = 0;
+//         for(int i = 1; i < n; ++i) {
+//             vector<int> new_bits = bits[i];
+//             bool is_valid = true;
+//             for (int new_bit : new_bits) {
+//                 if (st.find(new_bit) != st.end()) {
+//                     is_valid = false;
+//                     break;
+//                 }
+//             }
+//             while (!is_valid) {
+//                 for (int rm : bits[l_o]) 
+//                     st.erase(rm);
+//                     l_o++;
+//                     is_valid = true;
+//                     for (int new_bit : new_bits) {
+//                         if (st.find(new_bit) != st.end()) {
+//                             is_valid = false;
+//                             break;
+//                         }
+//                 }
+//             }
+//             for (int new_bit : new_bits) 
+//                 st.insert(new_bit);
+//             max_length = max(max_length, i - l_o + 1);
+//         }
+
+//         return max_length;
+//     }
+// };
