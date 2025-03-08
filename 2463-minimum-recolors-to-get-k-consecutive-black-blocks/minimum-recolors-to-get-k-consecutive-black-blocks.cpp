01b0f26c1b7600524bcaa37236c7572540a0a02e
Aadarsh Tyagi <112549335+Aadi1909@users.noreply.github.com>
2025-03-08 16:20:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.01%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -3,15 +3,16 @@ public:
     int minimumRecolors(string block, int k) {
         int answer = 1e9;
         int black = 0;
-        for (int i = 0; i < k; ++i) {
+        for (int i = 0; i < block.length(); ++i) {
+            if (i < k) {
+                if (block[i] == 'B') black++;
+                continue;
+            }
+            answer = min(answer, k - black);
+            if (block[i - k] == 'B') black--;
             if (block[i] == 'B') black++;
         }
         answer = min(answer, k - black);
-        for (int i = k; i < block.length(); ++i) {
-            if (block[i - k] == 'B') black--;
-            if (block[i] == 'B') black++;
-            answer = min(answer, k - black);
-        }
         return answer;
     }
 };
