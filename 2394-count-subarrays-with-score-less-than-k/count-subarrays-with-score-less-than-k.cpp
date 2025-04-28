15d7828b886af9db99ad9119dda8458da00d61eb
Aadarsh Tyagi <112549335+Aadi1909@users.noreply.github.com>
2025-04-29 01:30:42 +0530

    Time: 43 ms (5.78%) | Memory: 108 MB (8.93%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
-  long long countSubarrays(vector<int>& nums, long long k) {
-    long long answer = 0;
-    int low = 0, size = 0;
-    long long sum = 0;
-    for (int i = 0; i < nums.size(); ++i) {
-      sum += nums[i];answer += (size - 1) * size / 2;
-      while (low <= i && sum * (i - low + 1) >= k) {
-        sum -= nums[low];
-        low++;
-      }
-      answer += (i - low + 1);
+    long long countSubarrays(vector<int>& nums, long long k) {
+        long long answer = 0, sum = 0;
+        // int low = 0;
+        // for (int i = 0; i < nums.size(); ++i) {
+        //   sum += nums[i];
+        //   while (low <= i && sum * (i - low + 1) >= k) {
+        //     sum -= nums[low];
+        //     low++;
+        //   }
+        //   answer += (i - low + 1);
+        // }
+        int n = nums.size();
+        vector<long long> prefix(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        for (int r = 0; r < n; ++r) {
+            int left = 0, right = r;
+            int l_valid = r + 1;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                long long sum = prefix[r + 1] - prefix[mid];
+                long long length = r - mid + 1;
+                if (sum * length < k) {
+                    l_valid = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            answer += (r - l_valid + 1);
+        }
+        return answer;
     }
-    return answer; 
-  }
 };
