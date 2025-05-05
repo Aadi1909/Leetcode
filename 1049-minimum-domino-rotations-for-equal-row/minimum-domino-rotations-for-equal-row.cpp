800f1836cb0edd33474a5bc3a0c08234f0f7b4a2
Aadarsh Tyagi <112549335+Aadi1909@users.noreply.github.com>
2025-05-05 20:47:03 +0530

    Time: 2 ms (72.11%) | Memory: 115.7 MB (6.66%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        // first create the map to count if it's possible or not to make all equal
-        map<int, int> dominoValues, topTrack, bottomTrack;
-        map<int, set<int>> counting;
+    int check(int x, vector<int>& tops, vector<int>& bottoms) {
+        int rotationsTop = 0, rotationsBottom = 0;
         for (int i = 0; i < tops.size(); ++i) {
-            dominoValues[tops[i]]++;
-            topTrack[tops[i]]++;
-            counting[tops[i]].insert(i);
+            if (tops[i] != x && bottoms[i] != x) {
+                return -1;
+            } else if (tops[i] != x) {
+                rotationsTop++;
+            } else if (bottoms[i] != x) {
+                rotationsBottom++;
+            }
         }
-        for (int j = 0; j < bottoms.size(); ++j) {
-            dominoValues[bottoms[j]]++;
-            bottomTrack[bottoms[j]]++;
-            counting[bottoms[j]].insert(j);
-        }
-        vector<pair<int, int>> pairs(dominoValues.begin(), dominoValues.end());
-        sort(pairs.begin(), pairs.end(), [](auto& x, auto& y){
-                if (x.second < y.second) return true;
-                return false;
-        });
-        int len = pairs.size();
-        int topMin = 1e9, bottomMin = 1e9;
-        int sz = tops.size();
-        for (int i = len - 1; i >= 0 && counting[pairs[i].first].size() == sz; --i) {
-            int topVal = topTrack[pairs[i].first];
-            int bottomVal = bottomTrack[pairs[i].first];
-            topMin = min(topMin, abs(topVal - (int)tops.size()));
-            bottomMin = min(bottomMin, abs(bottomVal - (int)bottoms.size()));
-        }
-        int answer = min(topMin, bottomMin);
-        return answer == 1e9 ? -1 : answer;
+        return min(rotationsTop, rotationsBottom);
+    }
+
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int result = check(tops[0], tops, bottoms);
+        if (result != -1 || tops[0] == bottoms[0]) return result;
+        return check(bottoms[0], tops, bottoms);
     }
 };
