2b623585e336a3173a20bb5e94dce6bfc325d9b7
Aadarsh Tyagi <112549335+Aadi1909@users.noreply.github.com>
2025-05-11 17:15:00 +0530

    Time: 191 ms (15.61%) | Memory: 51.4 MB (35.67%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -1,26 +1,22 @@
-from typing import List
-
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         if not points:
             return 0
-
-        # Sort based on start point
+        #Find the overlapping intervals
         points.sort()
-        
-        arrows = 1
         initialx = points[0][0]
         initialy = points[0][1]
-        
+        arrows = 1
         for i in range(1, len(points)):
-            if points[i][0] <= initialy:
-                # Overlap: narrow the range
-                initialx = max(initialx, points[i][0])
+            if initialx <= points[i][0] <= initialy or initialx <= points[i][1] <= initialy:
+                initialx = min(initialx, points[i][0])
                 initialy = min(initialy, points[i][1])
+            
             else:
-                # No overlap: need new arrow
-                arrows += 1
                 initialx = points[i][0]
                 initialy = points[i][1]
+                arrows += 1
 
         return arrows
+
+
