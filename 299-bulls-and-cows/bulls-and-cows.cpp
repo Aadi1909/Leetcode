431633450d4d520a6357db94e1ce4753c834cd82
Aadarsh Tyagi <112549335+Aadi1909@users.noreply.github.com>
2025-06-09 22:59:25 +0530

    Time: 2 ms (36.00%) | Memory: 8.9 MB (57.93%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.cpp
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        unordered_map<int, set<int>> mp1;
-        unordered_map<int, pair<int, int>> mp2;            
-        unordered_map<int, int> mp3; 
-        for (int i = 0; i < secret.length(); ++i) {
-            mp1[secret[i] - '0'].insert(i);
-            mp3[secret[i] - '0']++;
-        }
         int bulls = 0, cows = 0;
-        for (int i = 0; i < guess.length(); ++i) {
-            int digit = guess[i] - '0';
-            if (mp1.find(digit) == mp1.end()) continue;
-
-            if (mp1[digit].find(i) != mp1[digit].end()) {
-                mp2[digit].first += 1;
+        vector<int> s_count(10, 0), g_count(10, 0);
+        for (int i = 0; i < secret.length(); ++i) {
+            if (secret[i] == guess[i]) {
+                bulls++;
             } else {
-                mp2[digit].second += 1; 
+                s_count[secret[i] - '0']++;
+                g_count[guess[i] - '0']++;
             }
         }
-
-        for (auto& m2 : mp2) {
-            int digit = m2.first;
-            int digit_bulls = m2.second.first;
-            int digit_non_bull_guesses = m2.second.second;
-            bulls += digit_bulls;
-            int available_for_cows = mp3[digit] - digit_bulls;
-            cows += min(available_for_cows, digit_non_bull_guesses);
+        for (int i = 0; i < 10; ++i) {
+            cows += min(s_count[i], g_count[i]);
         }
-
-        string answer = to_string(bulls) + "A" + to_string(cows) + "B";
-        return answer;
+        return to_string(bulls) + "A" + to_string(cows) + "B";
     }
 };
